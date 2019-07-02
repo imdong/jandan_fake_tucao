8bf2bb995473043255bcec125eeb10c6a037d26f
ImDong <www@qs5.org>
2019-07-03 02:00:59 +0800

    测试 版本号

--- jandan_fake_tucao.user.js
+++ jandan_fake_tucao.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         煎蛋外挂吐槽
 // @namespace    http://qs5.org/?jandan_fake_tucao
-// @version      0.3
+// @version      0.4
 // @description  不能吐槽怎么活？
 // @author       ImDong
 // @match        *://jandan.net/*
